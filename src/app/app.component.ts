688cb1d21bf9b000df99e0c2327bcfd522086ca6
andrew.fabrizio <andrew.m.fabrizio@gmail.com>
2017-11-03 20:42:38 -0700

    Project setup using the cli. Build home page and added some formatting and validators to the phone input field.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private title = 'weather-alerts';
+  public phone = '';
+  public valid = false;
+  public mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  getAlerts(e: Event) {
+    console.log(this.numsOnly(this.phone));
+  }
+
+  validate() {
+    if (this.numsOnly(this.phone).length === 10) {
+      this.valid = true;
+    } else {
+      this.valid = false;
+    }
+  }
+
+  numsOnly(formatted: string): string {
+    return formatted.replace(new RegExp('[^0-9]', 'g'), '');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TextMaskModule } from 'angular2-text-mask';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    TextMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
